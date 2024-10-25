8063daf5d232a0c4183fa382a417a978bb2f2645
Dexton Anderson <danderson@ihmc.org>
2024-10-25 16:14:44 -0500

    Add additional demo configuration files

---
+++ src/main/java/us/ihmc/rdx/examples/RDXApartmentViewer.java
@@ -0,0 +1,11 @@
+package us.ihmc.rdx.examples;
+
+import us.ihmc.rdx.RDXModelViewer;
+
+public class RDXApartmentViewer
+{
+   public static void main(String[] args)
+   {
+      new RDXModelViewer("apartment.g3dj");
+   }
+}
